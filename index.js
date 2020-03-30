7ed96666d080a259eb885ccb40e0986ea040abe0
kdw9502 <kdw9502@gmail.com>
2020-03-30 16:28:18 +0900

    add sql test code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 const port = 8000;
@@ -10,22 +10,20 @@ const port = 8000;
 
 const get_callback = function (req, res) {
     let json_data = req.query;
-    json_data = add_additional_date(json_data,req);
+    json_data = add_additional_date(json_data, req);
 
     res.send(json_data)
 };
 
-const post_callback = function (req,res)
-{
+const post_callback = function (req, res) {
     console.log(req.body)
     let json_data = req.body;
-    json_data = add_additional_date(json_data,req);
+    json_data = add_additional_date(json_data, req);
 
     res.send(json_data)
 };
 
-const add_additional_date = function (original_json,req)
-{
+const add_additional_date = function (original_json, req) {
     let json_data = typeof original_json === 'string' ? JSON.parse(original_json) : original_json;
     json_data["email"] = "kdw9502@gmail.com";
     json_data["stuno"] = "20141494";
@@ -35,13 +33,33 @@ const add_additional_date = function (original_json,req)
     let hour_minute_second = now_date[1].split('.')[0];
 
     json_data["time"] = year_month_day + " " + hour_minute_second;
-    json_data["ip"] = req.ip.replace(/^.*:/,'');
+    json_data["ip"] = req.ip.replace(/^.*:/, '');
 
     return json_data
+};
+
+const mysql = require('mysql');
+const mysql_test = function () {
+    var connection = mysql.createConnection(
+        {
+            host: '18.232.93.89',
+            user: 'kdw9502',
+            password: 'rkdehddnr1',
+            database: 'kdw9502_db'
+        }
+    )
+    connection.connect();
+
+    connection.query('SELECT * from test;', function (error, results, fields) {
+        if (error) throw error;
+        console.log('The solution is: ', results);
+    })
+    connection.end();
 };
 
 app.post('/', post_callback);
 app.get('/get', get_callback);
 app.get('/', get_callback);
-
+app.get('/test', mysql_test);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
